d26eec84b71b56b29431c7876ceac71d4b8b6d1a
foreverxiongtao <George@zhonghaiweb.com>
2018-05-09 14:38:33 +0800

    index.wxml

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,17 +7,34 @@ Page({
     motto: 'Hello World xiong tao',
     userInfo: {},
     hasUserInfo: false,
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
+    // imgUrls: [
+    //   'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+    //   'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+    //   'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    // ],
     indicatorDots: false,
     autoplay: false,
     interval: 5000,
-    duration: 500
+    duration: 500,
+    imgUrls:null
   },
 
+  getImageList:function(){
+    var context=this
+    wx.request({
+      url: app.globalData["apiHost"]+"/index",
+      method:"get",
+      success:function(res){
+        console.log(res);
+        context.setData({
+          imgUrls:res.data
+        })
+      },
+      error:function(){
+        console.log("error")
+      }
+    })
+  },
 
   //事件处理函数
   bindViewTap: function () {
@@ -26,6 +43,7 @@ Page({
     })
   },
   onLoad: function () {
+    this.getImageList()
   },
   showBigImage:function(e){
     var index=e.currentTarget.dataset["index"];
